d881e9b504637dbf5f8a2fcb7c5f7d0f10ad24ec
Pulipuli Chen <blog@pulipuli.info>
2019-10-13 19:27:29 +0800

    1013 上傳了

---
+++ 10/13-vue/vue-slim-tabs.vue
@@ -0,0 +1,31 @@
+<template>
+  <tabs>
+    <tab title="Vue">
+      This is {{ tabName1 }}
+    </tab>
+    <tab title="React">
+      This is {{ tabName2 }}
+    </tab>
+    <tab title="Svelte">
+      This is {{ tabName3 }}
+    </tab>
+  </tabs>
+</template>
+
+<script>
+module.exports = {
+  data: () => {
+    return {
+      tabName1: 'Vue',
+      tabName2: 'React',
+      tabName3: 'Svelte'
+    }
+  }
+}
+</script>
+
+<style>
+  .vue-tabs {
+    border: 1px solid gray;
+  }
+</style>
